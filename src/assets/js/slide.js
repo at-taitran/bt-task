ae71031159a57a1de3111cd3d494e6043e63aa78
MBA0055 <mba0055@MBA0055.local>
2020-11-10 15:43:58 +0700

    Update module and animate mixin.

--- src/assets/js/slide.js
+++ src/assets/js/slide.js
@@ -1,18 +1,21 @@
+const { fn } = require("@angular/compiler/src/output/output_ast");
 
-var slideIndex = 1;
-showDivs(slideIndex);
+window.onload  = function(){
+  let slideIndex = 1;
+  showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  function plusDivs(n) {
+    showDivs(slideIndex += n);
   }
-  x[slideIndex-1].style.display = "block";  
+
+  function showDivs(n) {
+    let x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "block";  
+  }
 }
+
